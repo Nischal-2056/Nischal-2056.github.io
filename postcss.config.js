80c0a61f17097e89bf9d441b1929a1d7d00a3655
Nischal-2056 <nischal.bh23@gmail.com>
2025-08-23 22:59:20 +0545

    final push before edit

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,7 @@
-export const plugins = {
-  tailwindcss: {},
-  autoprefixer: {},
+// postcss.config.ts
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
+export default {
+  plugins: [tailwindcss, autoprefixer],
 };
